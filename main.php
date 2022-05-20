2b43ed338fba40c153c7b0af87d2a0e18c4e2b55
Luiz Carlos Coelho Conde <luizcoelhoc1@gmail.com>
2022-05-19 21:17:57 -0300

    removing array logic from results, bitwise use instead

--- main.php
+++ main.php
@@ -10,19 +10,38 @@
         ];
     }
 
-    function somador_n_bits($n1, $n2, $n) {
-        $result = [];
-        $v1 = 0;
-        $n--;
-        while ($n > 0) {
-            $a = substr($n1, $n, 1);
-            $b = substr($n2, $n, 1);
-            $x = somador_1_bit($a, $b, $v1);
-            array_push($result, $x[RESULT]);
-            $v1 = $x[COUT];
-            $n--;
+    function int_append(&$item, $subject, $n_bits = 1) {
+        $item = $item << $n_bits;
+        $item = $item | $subject;
+    }
+
+    function invert_binary($n, $n_bits) {
+        $result = 0;
+        $n = dec_to_bin($n, $n_bits);
+        $count = $n_bits;
+        while ($count > 0) {
+            $i = $count-1;
+            $x = substr($n, $i, 1);
+            int_append($result, intval($x));
+            $count--;
         }
-        return bindec(implode("", array_reverse($result)));
+        return $result;
+    }
+
+    function somador_n_bits($n1, $n2, $n) {
+        $result = 0;
+        $v1 = 0;
+        $count = $n-1;
+        while ($count > 0) {
+            $a = substr($n1, $count, 1);
+            $b = substr($n2, $count, 1);
+            $x = somador_1_bit($a, $b, $v1);
+            int_append($result,  $x[RESULT]);
+            $v1 = $x[COUT];
+            $count--;
+        }
+        $result <<= 1; //removing first 0
+        return invert_binary($result, $n);
     }
 
     function dec_to_bin ($n, $size=32) {
@@ -34,6 +53,6 @@
         $n2 = dec_to_bin($n2, $size);
         return somador_n_bits($n1, $n2, $size);
     }
-    
-    echo add(79,7);
+
+    echo add(5, 7);
 ?>
