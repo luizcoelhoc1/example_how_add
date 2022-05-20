79c14af6c794c82455d5a035e0c329f40db70fa9
Luiz Carlos Coelho Conde <luizcoelhoc1@gmail.com>
2022-05-19 22:40:27 -0300

    replacing representation for numbers
    using int and bitwise as representation for sum two numbers instead strings

--- main.php
+++ main.php
@@ -1,7 +1,42 @@
 <?php
-
     define("COUT", 0);
     define("RESULT", 1);
+
+    function append_intbinary(&$item, $subject, $n_bits = 1) {
+        $item = $item << $n_bits;
+        $item = $item | $subject;
+    }
+
+    function invert_intbinary($n, $n_bits) {
+        $result = 0;
+        $count = $n_bits;
+        while ($count > 0) {
+            $i = $count-1;
+            $x = get_sub_intbinary($n, $n_bits, $i, 1);
+            append_intbinary($result, intval($x));
+            $count--;
+        }
+        return $result;
+    }
+
+    function shift_one(&$n, $qntty) {
+        while ($qntty > 0) {
+            $n <<= 1;
+            $n = $n | 1;
+            $qntty--;
+        }
+    }
+
+    function get_sub_intbinary($n, $n_bits, $index, $size_sub_int = 1) {
+        $result = 0;
+        shift_one($result, $size_sub_int);
+        $doesnt_metter = $n_bits - ($n_bits - $index);
+        $end_zeros = $n_bits - $size_sub_int - $doesnt_metter;
+        $result <<= $end_zeros;
+        $result = $n & $result;
+        $result >>= $end_zeros;
+        return $result;
+    }
 
     function somador_1_bit($a, $b, $cin) {
         return [
@@ -10,49 +45,25 @@
         ];
     }
 
-    function int_append(&$item, $subject, $n_bits = 1) {
-        $item = $item << $n_bits;
-        $item = $item | $subject;
-    }
-
-    function invert_binary($n, $n_bits) {
-        $result = 0;
-        $n = dec_to_bin($n, $n_bits);
-        $count = $n_bits;
-        while ($count > 0) {
-            $i = $count-1;
-            $x = substr($n, $i, 1);
-            int_append($result, intval($x));
-            $count--;
-        }
-        return $result;
-    }
-
     function somador_n_bits($n1, $n2, $n) {
         $result = 0;
         $v1 = 0;
         $count = $n-1;
         while ($count > 0) {
-            $a = substr($n1, $count, 1);
-            $b = substr($n2, $count, 1);
+            $a = get_sub_intbinary($n1, $n, $count, 1);
+            $b = get_sub_intbinary($n2, $n, $count, 1);
             $x = somador_1_bit($a, $b, $v1);
-            int_append($result,  $x[RESULT]);
+            append_intbinary($result,  $x[RESULT]);
             $v1 = $x[COUT];
             $count--;
         }
         $result <<= 1; //removing first 0
-        return invert_binary($result, $n);
-    }
-
-    function dec_to_bin ($n, $size=32) {
-        return str_pad(decbin($n), $size, '0', STR_PAD_LEFT);
+        return invert_intbinary($result, $n);
     }
 
     function add($n1, $n2, $size = 32) {
-        $n1 = dec_to_bin($n1, $size);
-        $n2 = dec_to_bin($n2, $size);
         return somador_n_bits($n1, $n2, $size);
     }
 
-    echo add(5, 7);
+    echo add(67, 50);
 ?>
